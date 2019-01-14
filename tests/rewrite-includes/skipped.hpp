44add3fbedb5cb1bdcbbffbce2ab3473d55b5cdc
Philip Salzmann <philip.salzmann@uibk.ac.at>
2019-01-14 08:51:49 +0000

    Add test case for skipped includes in rewriter

---
+++ tests/rewrite-includes/skipped.hpp
@@ -0,0 +1,7 @@
+#ifndef SKIPPED_HPP
+#define SKIPPED_HPP
+
+// Should only be defined once in rewritten output file
+struct defined_once {};
+
+#endif
---
+++ tests/rewrite-includes/test_skipped_includes.cpp
@@ -0,0 +1,10 @@
+// This should be rewritten (inlined) and the directive commented out
+#include "skipped.hpp"
+
+// This directive should be commented out
+#include "skipped.hpp"
+
+int main()
+{
+  return 0;
+}

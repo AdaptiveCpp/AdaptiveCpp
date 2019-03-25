fc99c34657fde03b4743e7e8d84e2d0e6bf965cb
Philip Salzmann <philip.salzmann@uibk.ac.at>
2019-03-25 10:25:11 +0000

    Fix vec swizzle compilation on Clang, add vec API tests
    This adds __host__ and __device__ attributes to the cl::sycl::vec
    swizzle functions, as without them compilation fails on Clang (as is to
    be expected).
    
    The existing smoke tests are converted to unit tests.

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -29,6 +29,7 @@
 #define BOOST_TEST_MODULE hipsycl unit tests
 #include <boost/test/unit_test.hpp>
 
+#define SYCL_SIMPLE_SWIZZLES
 #include <CL/sycl.hpp>
 
 BOOST_AUTO_TEST_CASE(basic_single_task) {
@@ -241,5 +242,82 @@ BOOST_AUTO_TEST_CASE(task_graph_synchronization) {
   for(size_t i = num_elements; i < num_elements; ++i) {
     BOOST_REQUIRE(result[i] == 2 * i);
   }
+}
+
+BOOST_AUTO_TEST_CASE(vec_api) {
+  cl::sycl::queue queue;
+  cl::sycl::buffer<float, 1> results{60};
+
+  queue.submit([&](cl::sycl::handler& cgh) {
+    auto acc = results.get_access<cl::sycl::access::mode::discard_write>(cgh);
+    cgh.single_task([=]() {
+      size_t offset = 0;
+      const auto store_results = [=, &offset](
+        const std::initializer_list<float>& results) {
+          for(auto r : results) {
+            acc[offset++] = r;
+          }
+        };
+
+      const cl::sycl::vec<float, 4> v1(1.0f);
+      store_results({v1.x(), v1.y(), v1.z(), v1.w()});
+
+      const cl::sycl::vec<float, 8> v2(1.f, 2.f, 4.f, v1, 8.f);
+      store_results({v2.s0(), v2.s1(), v2.s2(), v2.s3(),
+        v2.s4(), v2.s5(), v2.s6(), v2.s7()});
+
+      // Broadcasting math functions over vector elements
+      const auto v3 = cl::sycl::log2(v2);
+      store_results({v3.s0(), v3.s1(), v3.s2(), v3.s3(),
+        v3.s4(), v3.s5(), v3.s6(), v3.s7()});
+
+      const auto v4 = cl::sycl::fma(v2, v2, v2);
+      store_results({v4.s0(), v4.s1(), v4.s2(), v4.s3(),
+        v4.s4(), v4.s5(), v4.s6(), v4.s7()});
+
+      const auto v5 = v4 + v4;
+      store_results({v5.s0(), v5.s1(), v5.s2(), v5.s3(),
+        v5.s4(), v5.s5(), v5.s6(), v5.s7()});
+
+      const auto v6 = cl::sycl::cos(v2) < cl::sycl::sin(v2);
+      store_results({(float)v6.s0(), (float)v6.s1(), (float)v6.s2(), (float)v6.s3(),
+        (float)v6.s4(), (float)v6.s5(), (float)v6.s6(), (float)v6.s7()});
+
+      // Swizzles!
+      const cl::sycl::vec<float, 4> v7 = v2.lo();
+      store_results({v7.x(), v7.y(), v7.z(), v7.w()});
+
+      const cl::sycl::vec<float, 4> v8 = v7.wzyx();
+      store_results({v8.x(), v8.y(), v8.z(), v8.w()});
+
+      cl::sycl::vec<float, 4> v9(1.f);
+      v9.xwyz() = v7.xxyy();
+      store_results({v9.x(), v9.y(), v9.z(), v9.w()});
+
+      // Nested swizzles
+      cl::sycl::vec<float, 4> v10(4.f);
+      v10.zxyw().lo() = v7.xxyy().hi();
+      store_results({v10.x(), v10.y(), v10.z(), v10.w()});
+    });
+  });
+
+  auto acc = results.get_access<cl::sycl::access::mode::read>();
+  size_t offset = 0;
+  const auto verify_results = [&](const std::initializer_list<float>& expected) {
+    for(auto e : expected) {
+      BOOST_TEST(acc[offset++] == e);
+    }
+  };
+
+  verify_results({1.f, 1.f, 1.f, 1.f});                         // v1
+  verify_results({1.f, 2.f, 4.f, 1.f, 1.f, 1.f, 1.f, 8.f});     // v2
+  verify_results({0.f, 1.f, 2.f, 0.f, 0.f, 0.f, 0.f, 3.f});     // v3
+  verify_results({2.f, 6.f, 20.f, 2.f, 2.f, 2.f, 2.f, 72.f});   // v4
+  verify_results({4.f, 12.f, 40.f, 4.f, 4.f, 4.f, 4.f, 144.f}); // v5
+  verify_results({1.f, 1.f, 0.f, 1.f, 1.f, 1.f, 1.f, 1.f});     // v6
+  verify_results({1.f, 2.f, 4.f, 1.f});                         // v7
+  verify_results({1.f, 4.f, 2.f, 1.f});                         // v8
+  verify_results({1.f, 2.f, 2.f, 1.f});                         // v9
+  verify_results({2.f, 4.f, 2.f, 4.f});                         // v10
 }
 

77fda7738dc7c92a49ca5951a1c1f2044cb5a48d
Philip Salzmann <philip.salzmann@uibk.ac.at>
2019-04-02 12:46:34 +0200

    Add basic support for explicit copy operations
    This adds support for the most important explicit copy operations,
    namely transferring data between all combinations of host and device
    accessors for global buffers, as well as raw and shared host pointers.
    1D and 2D copying is supported on all platforms, while 3D copying is
    currently only supported on CUDA.
    
    In order to support copying of subranges at arbitrary locations within
    source and target buffers, this additionally adds proper handling of
    ranges and offsets for accessors.
    
    A known limitation for now is that since hipSYCL currently only tracks
    buffer versions for entire buffers, performing an explicit copy using a
    discard_* access mode into a subrange of a buffer will invalidate the
    entire buffer range.

--- include/CL/sycl/accessor.hpp
+++ include/CL/sycl/accessor.hpp
@@ -51,6 +51,12 @@ class buffer;
 
 class handler;
 
+template<typename dataT, int dimensions,
+         access::mode accessmode,
+         access::target accessTarget,
+         access::placeholder isPlaceholder>
+class accessor;
+
 namespace detail {
 namespace buffer {
 
@@ -91,6 +97,22 @@ void* obtain_device_access(buffer_ptr buff,
                            sycl::handler& cgh,
                            access::mode access_mode);
 
+template<typename dataT, int dimensions,
+         access::mode accessmode,
+         access::target accessTarget = access::target::global_buffer,
+         access::placeholder isPlaceholder = access::placeholder::false_t>
+sycl::range<dimensions> get_buffer_range(const sycl::accessor<dataT, dimensions,
+  accessmode, accessTarget, isPlaceholder>&);
+
+template<typename dataT, int dimensions,
+         access::mode accessmode,
+         access::target accessTarget = access::target::global_buffer,
+         access::placeholder isPlaceholder = access::placeholder::false_t>
+size_t get_pointer_offset(const sycl::accessor<dataT, dimensions,
+  accessmode, accessTarget, isPlaceholder>& acc)
+{
+  return detail::linear_id<dimensions>::get(acc.get_offset(), get_buffer_range(acc));
+}
 
 } // accessor
 
@@ -171,7 +193,9 @@ template<typename dataT, int dimensions,
          access::placeholder isPlaceholder = access::placeholder::false_t>
 class accessor : public detail::accessor_base
 {
-
+  friend range<dimensions> detail::accessor::get_buffer_range<dataT, dimensions,
+    accessmode, accessTarget, isPlaceholder>(
+    const sycl::accessor<dataT, dimensions, accessmode, accessTarget, isPlaceholder>&);
 public:
   using value_type = dataT;
   using reference = dataT &;
@@ -242,6 +266,7 @@ access::placeholder::true_t && (accessTarget == access::target::global_buffer
     {
       this->init_placeholder_accessor(bufferRef);
     }
+    _range = _buffer_range;
   }
 
   /* Available only when: (isPlaceholder == access::placeholder::false_t &&
@@ -259,6 +284,7 @@ access::target::constant_buffer)) && dimensions > 0 */
     : detail::accessor_base{detail::buffer::get_buffer_impl(bufferRef)}
   {
     this->init_device_accessor(bufferRef, commandGroupHandlerRef);
+    _range = _buffer_range;
   }
 
   /// Creates an accessor for a partial range of the buffer, described by an offset
@@ -292,6 +318,8 @@ access::target::constant_buffer)) && dimensions > 0 */
     {
       this->init_placeholder_accessor(bufferRef);
     }
+    _range = accessRange;
+    _offset = accessOffset;
   }
 
   /* Available only when: (isPlaceholder == access::placeholder::false_t &&
@@ -310,13 +338,17 @@ access::target::constant_buffer)) && dimensions > 0 */
     : detail::accessor_base{detail::buffer::get_buffer_impl(bufferRef)}
   {
     this->init_device_accessor(bufferRef, commandGroupHandlerRef);
+    _range = accessRange;
+    _offset = accessOffset;
   }
 
   __host__ __device__
   accessor(const accessor& other)
     : detail::accessor_base{other},
       _ptr{other._ptr},
-      _range{other._range}
+      _buffer_range{other._buffer_range},
+      _range{other._range},
+      _offset{other._offset}
   {}
 
   __host__ __device__
@@ -324,7 +356,9 @@ access::target::constant_buffer)) && dimensions > 0 */
   {
     detail::accessor_base::operator=(other);
     _ptr = other._ptr;
+    _buffer_range = other._buffer_range;
     _range = other._range;
+    _offset = other._offset;
     return *this;
   }
 
@@ -369,10 +403,9 @@ access::target::constant_buffer)) && dimensions > 0 */
   template<int D = dimensions,
            typename = std::enable_if_t<(D > 0)>>
   __host__ __device__
-  range<dimensions> get_offset() const
+  id<dimensions> get_offset() const
   {
-    // ToDo: Properly implement access offsets
-    return range<dimensions>{};
+    return _offset;
   }
   /* Available only when: (accessMode == access::mode::write || accessMode ==
 access::mode::read_write || accessMode == access::mode::discard_write ||
@@ -403,8 +436,7 @@ accessMode == access::mode::discard_read_write) && dimensions > 0) */
   __host__ __device__
   dataT &operator[](id<dimensions> index) const
   {
-
-    return _ptr[detail::linear_id<dimensions>::get(index, _range)];
+    return _ptr[detail::linear_id<dimensions>::get(index, _buffer_range)];
   }
 
   /* Available only when: (accessMode == access::mode::write || accessMode ==
@@ -441,7 +473,7 @@ accessMode == access::mode::discard_read_write) && dimensions == 1) */
            typename = std::enable_if_t<(D > 0) && (M == access::mode::read)>>
   __host__ __device__
   dataT operator[](id<dimensions> index) const
-  { return _ptr[detail::linear_id<dimensions>::get(index, _range)]; }
+  { return _ptr[detail::linear_id<dimensions>::get(index, _buffer_range)]; }
 
   /* Available only when: accessMode == access::mode::read && dimensions == 1 */
   template<int D = dimensions,
@@ -472,6 +504,7 @@ accessMode == access::mode::discard_read_write) && dimensions == 1) */
     // Create sub accessor for slice at _ptr[index][...]
     accessor<dataT, dimensions-1, accessmode, accessTarget, isPlaceholder> sub_accessor;
     sub_accessor._range = detail::range::omit_first_dimension(this->_range);
+    sub_accessor._buffer_range = _buffer_range;
     sub_accessor._ptr = this->_ptr + index * sub_accessor._range.size();
 
     return sub_accessor;
@@ -482,7 +515,7 @@ accessMode == access::mode::discard_read_write) && dimensions == 1) */
            typename = std::enable_if_t<T==access::target::host_buffer>>
   dataT *get_pointer() const
   {
-    return const_cast<dataT*>(this->_ptr);
+    return const_cast<dataT*>(_ptr);
   }
 
   /* Available only when: accessTarget == access::target::global_buffer */
@@ -491,7 +524,7 @@ accessMode == access::mode::discard_read_write) && dimensions == 1) */
   __host__ __device__
   global_ptr<dataT> get_pointer() const
   {
-    return global_ptr<dataT>{_ptr};
+    return global_ptr<dataT>{const_cast<dataT*>(_ptr)};
   }
 
   /* Available only when: accessTarget == access::target::constant_buffer */
@@ -500,7 +533,7 @@ accessMode == access::mode::discard_read_write) && dimensions == 1) */
   __host__ __device__
   constant_ptr<dataT> get_pointer() const
   {
-    return constant_ptr<dataT>{_ptr};
+    return constant_ptr<dataT>{const_cast<dataT*>(_ptr)};
   }
 private:
   template<class Buffer_type>
@@ -513,7 +546,7 @@ private:
                                                  commandGroupHandlerRef,
                                                  accessmode));
 
-    this->_range = detail::buffer::get_buffer_range(bufferRef);
+    this->_buffer_range = detail::buffer::get_buffer_range(bufferRef);
   }
 
   template<class Buffer_type>
@@ -524,7 +557,7 @@ private:
           detail::accessor::obtain_host_access(buff,
                                                accessmode));
 
-    this->_range = detail::buffer::get_buffer_range(bufferRef);
+    this->_buffer_range = detail::buffer::get_buffer_range(bufferRef);
   }
 
   template<class Buffer_type>
@@ -533,14 +566,16 @@ private:
     detail::buffer_ptr buff = detail::buffer::get_buffer_impl(bufferRef);
 
     this->_ptr = reinterpret_cast<pointer_type>(buff->get_buffer_ptr());
-    this->_range = detail::buffer::get_buffer_range(bufferRef);
+    this->_buffer_range = detail::buffer::get_buffer_range(bufferRef);
   }
 
   __host__ __device__
   accessor(){}
 
-  range<dimensions> _range;
   pointer_type _ptr;
+  range<dimensions> _buffer_range;
+  range<dimensions> _range;
+  id<dimensions> _offset;
 };
 
 /// Accessor specialization for local memory
@@ -685,6 +720,21 @@ private:
   const range<dimensions> _num_elements;
 };
 
+namespace detail {
+namespace accessor {
+
+template<typename dataT, int dimensions,
+         access::mode accessmode,
+         access::target accessTarget,
+         access::placeholder isPlaceholder>
+sycl::range<dimensions> get_buffer_range(const sycl::accessor<dataT, dimensions,
+  accessmode, accessTarget, isPlaceholder>& acc)
+{
+  return acc._buffer_range;
+}
+
+}
+}
 
 } // sycl
 } // cl

620bc14d7304ce1f6d9ea5aecbc299eb6b5dea80
PeterTh <peter@metaclassofnil.com>
2019-08-05 15:07:25 +0200

    Common functions: use HIPSYCL_KERNEL_TARGET annotation, M_PI, and HIPSYCL_ENABLE_IF_FLOATING_POINT macro where appropriate

--- include/CL/sycl/common_functions.hpp
+++ include/CL/sycl/common_functions.hpp
@@ -36,92 +36,105 @@
 namespace cl {
 namespace sycl {
 
-namespace detail {
-  template<class T>
-  constexpr T pi = T(3.1415926535897932385L);
-}
-
 template<typename T>
+HIPSYCL_KERNEL_TARGET
 inline auto clamp(T x, T minval, T maxval) {
   return fmin(fmax(x, minval), maxval);
 }
 template<typename T, typename D, std::enable_if_t<!std::is_same<D,T>::value, int> = 0>
+HIPSYCL_KERNEL_TARGET
 inline auto clamp(T x, D minval, D maxval) {
   return fmin(fmax(x, T{minval}), T{maxval});
 }
 
-template<typename T, std::enable_if_t<std::is_floating_point<T>::value, int> = 0>
+template<typename T, HIPSYCL_ENABLE_IF_FLOATING_POINT(T)>
+HIPSYCL_KERNEL_TARGET
 inline T degrees(T radians) {
-  return static_cast<T>(180)/detail::pi<T> * radians;
+  return T{180}/T{M_PI} * radians;
 }
-template<typename T, std::enable_if_t<!std::is_floating_point<T>::value, int> = 0>
+template<typename T, typename E = typename T::element_type, std::enable_if_t<!std::is_floating_point<T>::value, int> = 0>
+HIPSYCL_KERNEL_TARGET
 inline T degrees(T radians) {
-  return static_cast<typename T::element_type>(180)/detail::pi<typename T::element_type> * radians;
+  return E{180}/E{M_PI} * radians;
 }
 
 template<typename T>
+HIPSYCL_KERNEL_TARGET
 inline auto max(T x, T y) {
   return fmax(x, y);
 }
 template<typename T, typename D, std::enable_if_t<!std::is_same<D,T>::value, int> = 0>
+HIPSYCL_KERNEL_TARGET
 inline auto max(T x, D y) {
   return fmax(x, T{y});
 }
 
 template<typename T>
+HIPSYCL_KERNEL_TARGET
 inline auto min(T x, T y) {
   return fmin(x, y);
 }
 template<typename T, typename D, std::enable_if_t<!std::is_same<D,T>::value, int> = 0>
+HIPSYCL_KERNEL_TARGET
 inline auto min(T x, D y) {
   return fmin(x, T{y});
 }
 
 template<typename T, typename D>
+HIPSYCL_KERNEL_TARGET
 inline auto mix(T x, T y, D a) {
   return x + (y - x) * a;
 }
 
-template<typename T, std::enable_if_t<std::is_floating_point<T>::value, int> = 0>
+template<typename T, HIPSYCL_ENABLE_IF_FLOATING_POINT(T)>
+HIPSYCL_KERNEL_TARGET
 inline T radians(T deg) {
-  return detail::pi<T>/static_cast<T>(180) * deg;
+  return T{M_PI}/T{180} * deg;
 }
-template<typename T, std::enable_if_t<!std::is_floating_point<T>::value, int> = 0>
+template<typename T, typename E = typename T::element_type, std::enable_if_t<!std::is_floating_point<T>::value, int> = 0>
+HIPSYCL_KERNEL_TARGET
 inline T radians(T deg) {
-  return detail::pi<typename T::element_type>/static_cast<typename T::element_type>(180) * deg;
+  return E{M_PI}/E{180} * deg;
 }
 
-template<typename T, std::enable_if_t<std::is_floating_point<T>::value, int> = 0>
+template<typename T, HIPSYCL_ENABLE_IF_FLOATING_POINT(T)>
+HIPSYCL_KERNEL_TARGET
 inline T step(T edge, T x) {
   return x < edge ? 0.0 : 1.0;
 }
 template<typename T, typename E = typename T::element_type, std::enable_if_t<!std::is_floating_point<T>::value, int> = 0>
+HIPSYCL_KERNEL_TARGET
 inline T step(T edge, T x) {
   return detail::binary_vector_operation(edge, x, [](E _e, E _x) { return step(_e, _x); });
 }
 template<typename D, typename T, typename E = typename T::element_type,
   std::enable_if_t<std::is_floating_point<D>::value && !std::is_floating_point<T>::value, int> = 0>
+HIPSYCL_KERNEL_TARGET
 inline T step(D edge, T x) {
   detail::transform_vector(x, [edge](E _x) { return step(edge, _x); });
   return x;
 }
 
-template<typename T, std::enable_if_t<std::is_floating_point<T>::value, int> = 0>
+template<typename T, HIPSYCL_ENABLE_IF_FLOATING_POINT(T)>
+HIPSYCL_KERNEL_TARGET
 inline T smoothstep(T edge0, T edge1, T x) {
   T t = clamp((x - edge0) / (edge1 - edge0), T{0}, T{1});
   return t * t * (T{3} - T{2} * t);
 }
 template<typename T, typename E = typename T::element_type>
+HIPSYCL_KERNEL_TARGET
 inline T smoothstep(T edge0, T edge1, T x) {
   T t = clamp((x - edge0) / (edge1 - edge0), E{0}, E{1});
   return t * t * (E{3} - E{2} * t);
 }
 template<typename D, typename T, std::enable_if_t<!std::is_same<D,T>::value, int> = 0>
+HIPSYCL_KERNEL_TARGET
 inline T smoothstep(D edge0, D edge1, T x) {
   return smoothstep(T{edge0}, T{edge1}, x);
 }
 
-template<typename T, std::enable_if_t<std::is_floating_point<T>::value, int> = 0>
+template<typename T, HIPSYCL_ENABLE_IF_FLOATING_POINT(T)>
+HIPSYCL_KERNEL_TARGET
 inline T sign(T x) {
   if(x > T{0}) return T{1};
   if(x < T{0}) return T{-1};
@@ -129,6 +142,7 @@ inline T sign(T x) {
   return x;
 }
 template<typename T, typename E = typename T::element_type, std::enable_if_t<!std::is_floating_point<T>::value, int> = 0>
+HIPSYCL_KERNEL_TARGET
 inline T sign(T x) {
   detail::transform_vector(x, [](E e) { return sign(e); });
   return x;

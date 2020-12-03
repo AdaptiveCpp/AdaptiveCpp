29182d872c5ae344edfb0dcb5e3853e336ed1f3d
Aksel Alpay <aksel.alpay@uni-heidelberg.de>
2020-12-03 18:39:26 +0100

    Use std::promise for omp_node_event instead of busy wait

--- include/hipSYCL/runtime/omp/omp_event.hpp
+++ include/hipSYCL/runtime/omp/omp_event.hpp
@@ -28,13 +28,39 @@
 #ifndef HIPSYCL_OMP_EVENT_HPP
 #define HIPSYCL_OMP_EVENT_HPP
 
-#include <atomic>
+#include <future>
+#include <chrono>
 
 #include "../event.hpp"
 
 namespace hipsycl {
 namespace rt {
 
+class signal_channel {
+public:
+  signal_channel() {
+    _shared_future = _promise.get_future().share();
+  }
+
+  void signal() {
+    _promise.set_value(true);
+  }
+
+  void wait() {
+    auto future = _shared_future;
+    future.wait();
+  }
+
+  bool has_signalled() const {
+    auto future = _shared_future;
+    return future.wait_for(std::chrono::seconds(0)) ==
+           std::future_status::ready;
+  }
+
+private:
+  std::promise<bool> _promise;
+  std::shared_future<bool> _shared_future;
+};
 
 class omp_node_event : public dag_node_event
 {
@@ -46,9 +72,10 @@ public:
   virtual bool is_complete() const override;
   virtual void wait() override;
 
-  std::shared_ptr<std::atomic<bool>> get_completion_flag() const;
+  std::shared_ptr<signal_channel> get_signal_channel() const;
 private:
-  std::shared_ptr<std::atomic<bool>> _is_complete;
+
+  std::shared_ptr<signal_channel> _signal_channel;
 };
 
 

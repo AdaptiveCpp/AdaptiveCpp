dd90ccb1c658ebdb50844fa3c291ea6ae67a2e6c
Joachim Meyer <joachim@joameyer.de>
2021-06-08 15:33:11 +0200

    Update pruning IR pass.
    This cleans up the function pruning and moves the globals pruning into an accordingly named pass.
    To support the new pass manager infrastructure which will be the default for Clang 13+.
    The new PM can be activated in Clang 11/12 with -fexperimental-new-pass-manager.
    The option -fpass-plugin= is already present in Clang 11.

---
+++ src/compiler/GlobalsPruningPass.cpp
@@ -0,0 +1,54 @@
+#include "hipSYCL/compiler/GlobalsPruningPass.hpp"
+
+namespace {
+bool canGlobalVariableBeRemoved(llvm::GlobalVariable *G) {
+  G->removeDeadConstantUsers();
+  return G->getNumUses() == 0;
+}
+
+void pruneUnusedGlobals(llvm::Module &M) {
+
+  HIPSYCL_DEBUG_INFO << " ****** Starting pruning of global variables ******" << std::endl;
+
+  std::vector<llvm::GlobalVariable *> VariablesForPruning;
+
+  for (auto G = M.global_begin(); G != M.global_end(); ++G) {
+    llvm::GlobalVariable *GPtr = &(*G);
+    if (canGlobalVariableBeRemoved(GPtr)) {
+      VariablesForPruning.push_back(GPtr);
+
+      HIPSYCL_DEBUG_INFO << "IR Processing: Pruning unused global variable from device code: " << G->getName().str()
+                         << std::endl;
+    }
+  }
+
+  for (auto G : VariablesForPruning) {
+    G->replaceAllUsesWith(llvm::UndefValue::get(G->getType()));
+    G->eraseFromParent();
+  }
+  HIPSYCL_DEBUG_INFO << "===> IR Processing: Pruning of globals complete, removed " << VariablesForPruning.size()
+                     << " global variable(s)." << std::endl;
+}
+
+} // namespace
+
+bool hipsycl::compiler::GlobalsPruningPassLegacy::runOnModule(llvm::Module &M) {
+  if (!CompilationStateManager::getASTPassState().isDeviceCompilation())
+    return false;
+
+  pruneUnusedGlobals(M);
+
+  return true;
+}
+
+llvm::PreservedAnalyses hipsycl::compiler::GlobalsPruningPass::run(llvm::Module &M, llvm::ModuleAnalysisManager &AM) {
+  if (!CompilationStateManager::getASTPassState().isDeviceCompilation())
+    return llvm::PreservedAnalyses::all();
+
+  pruneUnusedGlobals(M);
+
+  // todo: be a bit more granular
+  return llvm::PreservedAnalyses::none();
+}
+
+char hipsycl::compiler::GlobalsPruningPassLegacy::ID = 0;
--- src/compiler/HipsyclClangPlugin.cpp
+++ src/compiler/HipsyclClangPlugin.cpp
@@ -24,35 +24,46 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#include "hipSYCL/common/config.hpp"
+
+#include "hipSYCL/compiler/FrontendPlugin.hpp"
+#include "hipSYCL/compiler/GlobalsPruningPass.hpp"
 
 #include "clang/Frontend/FrontendPluginRegistry.h"
 
-#include "hipSYCL/compiler/FrontendPlugin.hpp"
-#include "hipSYCL/compiler/IR.hpp"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Pass.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
 
 namespace hipsycl {
 namespace compiler {
 // Register and activate passes
 
-static clang::FrontendPluginRegistry::Add<hipsycl::compiler::FrontendASTAction> 
-HipsyclFrontendPlugin {
-  "hipsycl_frontend", 
-  "enable hipSYCL frontend action"
-};
+static clang::FrontendPluginRegistry::Add<hipsycl::compiler::FrontendASTAction> HipsyclFrontendPlugin{
+    "hipsycl_frontend", "enable hipSYCL frontend action"};
 
-static void registerFunctionPruningIRPass(const llvm::PassManagerBuilder &,
-                                          llvm::legacy::PassManagerBase &PM) {
-  PM.add(new FunctionPruningIRPass{});
+static void registerGlobalsPruningPass(const llvm::PassManagerBuilder &, llvm::legacy::PassManagerBase &PM) {
+  PM.add(new GlobalsPruningPassLegacy{});
 }
 
 static llvm::RegisterStandardPasses
-  RegisterFunctionPruningIRPassOptLevel0(llvm::PassManagerBuilder::EP_EnabledOnOptLevel0,
-                                         registerFunctionPruningIRPass);
+    RegisterGlobalsPruningPassOptLevel0(llvm::PassManagerBuilder::EP_EnabledOnOptLevel0,
+                                          registerGlobalsPruningPass);
 
 static llvm::RegisterStandardPasses
-  RegisterFunctionPruningIRPassOptimizerLast(llvm::PassManagerBuilder::EP_OptimizerLast,
-                                             registerFunctionPruningIRPass);
+    RegisterGlobalsPruningPassOptimizerLast(llvm::PassManagerBuilder::EP_OptimizerLast, registerGlobalsPruningPass);
+
+#define HIPSYCL_STRINGIFY(V) #V
+#define HIPSYCL_PLUGIN_VERSION_STRING                                                                                  \
+  "v" HIPSYCL_STRINGIFY(HIPSYCL_VERSION_MAJOR) "." HIPSYCL_STRINGIFY(HIPSYCL_VERSION_MINOR)
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "hipSYCL Clang plugin", HIPSYCL_PLUGIN_VERSION_STRING, [](llvm::PassBuilder &PB) {
+            PB.registerPipelineStartEPCallback(
+                [](llvm::ModulePassManager &MPM) { MPM.addPass(hipsycl::compiler::GlobalsPruningPass{}); });
+          }};
+}
 
 } // namespace compiler
 } // namespace hipsycl
-

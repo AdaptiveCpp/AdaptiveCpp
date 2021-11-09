ce87a17598dad6f9e2db6ec6d805be2ac872431e
Aksel Alpay <aksel.alpay@uni-heidelberg.de>
2021-11-09 03:43:22 +0100

    [nvc++] Account for warpSize not being constexpr in nvc++

--- include/hipSYCL/glue/generic/hiplike/hiplike_kernel_launcher.hpp
+++ include/hipSYCL/glue/generic/hiplike/hiplike_kernel_launcher.hpp
@@ -288,11 +288,21 @@ private:
     using fallback_decomposition =
       nested_range<unknown_static_range, nested_range<static_range<1>>>;
 
+#ifdef HIPSYCL_LIBKERNEL_CUDA_NVCXX
+    // warpSize is not constexpr with nvc++. Hardcode to 32
+    // for now; the better solution would be to accept it as
+    // template parameter and the instantiate the kernel for
+    // various warp sizes.
+    constexpr int warp_size = 32;
+#else
+    constexpr int warp_size = warpSize;
+#endif
+
     if constexpr(Dim == 1) {
-      if constexpr(DivisorX % warpSize == 0) {
+      if constexpr(DivisorX % warp_size == 0) {
         using decomposition =
             nested_range<unknown_static_range,
-                         nested_range<static_range<warpSize>>>;
+                         nested_range<static_range<warp_size>>>;
 
         return decomposition{};
       } else {

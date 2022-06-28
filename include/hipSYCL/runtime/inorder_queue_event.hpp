06fec2f76d089a9674ed59fd2ac3461651be157f
Aksel Alpay <aksel.alpay@uni-heidelberg.de>
2022-06-28 04:51:12 +0200

    Document inorder_queue_event::request_backend_event() better

--- include/hipSYCL/runtime/inorder_queue_event.hpp
+++ include/hipSYCL/runtime/inorder_queue_event.hpp
@@ -38,6 +38,15 @@ class inorder_queue;
 template<class FineGrainedBackendEventT>
 class inorder_queue_event : public dag_node_event {
 public:
+  // Get fine-grained backend event that should be inserted into
+  // the queue as early as possible after the operation that it describes.
+  //
+  // This should only be invoked when absolutely necessary, since it can
+  // force event implementations that rely on interacting with the backend only
+  // lazily to actually insert events into a backend inorder queue.
+  //
+  // This function does not have to be thread-safe and should therefore only be
+  // invoked inside the runtime thread.
   virtual FineGrainedBackendEventT request_backend_event() = 0;
   virtual ~inorder_queue_event() {}
 };

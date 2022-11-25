5136d5960833eee2bc8b700f65f0a2405bd13a05
Aksel Alpay <aksel.alpay@uni-heidelberg.de>
2022-11-25 22:54:34 +0100

    [SSCP][HIP] Add SSCP support to HIP runtime backend

--- include/hipSYCL/runtime/hip/hip_code_object.hpp
+++ include/hipSYCL/runtime/hip/hip_code_object.hpp
@@ -31,6 +31,7 @@
 #include <vector>
 #include <string>
 
+#include "hipSYCL/glue/kernel_configuration.hpp"
 #include "hipSYCL/runtime/error.hpp"
 #include "hipSYCL/runtime/device_id.hpp"
 #include "hipSYCL/runtime/kernel_cache.hpp"
@@ -41,14 +42,21 @@ struct ihipModule_t;
 namespace hipsycl {
 namespace rt {
 
-
 class hip_executable_object : public code_object {
 public:
-  virtual ~hip_executable_object();
-  hip_executable_object(hcf_object_id origin, const std::string &target,
+  virtual ~hip_executable_object() {}
+  virtual ihipModule_t* get_module() const = 0;
+  virtual result get_build_result() const = 0;
+  virtual int get_device() const = 0;
+};
+
+class hip_multipass_executable_object : public hip_executable_object {
+public:
+  virtual ~hip_multipass_executable_object();
+  hip_multipass_executable_object(hcf_object_id origin, const std::string &target,
                         const std::string &hip_fat_binary, int device);
 
-  result get_build_result() const;
+  virtual result get_build_result() const override;
 
   virtual code_object_state state() const override;
   virtual code_format format() const override;
@@ -62,14 +70,52 @@ public:
 
   virtual bool contains(const std::string &backend_kernel_name) const override;
 
-  ihipModule_t* get_module() const;
-  int get_device() const;
+  virtual ihipModule_t* get_module() const override;
+  virtual int get_device() const override;
 private:
   result build(const std::string& hip_fat_binary);
 
   hcf_object_id _origin;
   std::string _target;
   result _build_result;
+  int _device;
+  ihipModule_t* _module;
+};
+
+class hip_sscp_executable_object : public hip_executable_object {
+public:
+  virtual ~hip_sscp_executable_object();
+  hip_sscp_executable_object(const std::string &hip_fat_binary,
+                             const std::string &target_arch,
+                             hcf_object_id source,
+                             const std::vector<std::string> &kernel_name,
+                             int device,
+                             const glue::kernel_configuration &config);
+
+  virtual result get_build_result() const override;
+
+  virtual code_object_state state() const override;
+  virtual code_format format() const override;
+  virtual backend_id managing_backend() const override;
+  virtual hcf_object_id hcf_source() const override;
+  virtual std::string target_arch() const override;
+  virtual compilation_flow source_compilation_flow() const override;
+
+  virtual std::vector<std::string>
+  supported_backend_kernel_names() const override;
+
+  virtual bool contains(const std::string &backend_kernel_name) const override;
+
+  virtual ihipModule_t* get_module() const override;
+  virtual int get_device() const override;
+private:
+  result build(const std::string& hip_fat_binary);
+
+  std::string _target;
+  hcf_object_id _origin;
+  std::vector<std::string> _kernel_names;
+  result _build_result;
+  glue::kernel_configuration::id_type _id;
   int _device;
   ihipModule_t* _module;
 };
--- src/runtime/hip/hip_code_object.cpp
+++ src/runtime/hip/hip_code_object.cpp
@@ -26,6 +26,7 @@
  */
 
 #include "hipSYCL/runtime/hip/hip_code_object.hpp"
+#include "hipSYCL/glue/kernel_configuration.hpp"
 #include "hipSYCL/runtime/device_id.hpp"
 #include "hipSYCL/runtime/error.hpp"
 #include "hipSYCL/runtime/hip/hip_device_manager.hpp"
@@ -35,13 +36,13 @@
 namespace hipsycl {
 namespace rt {
 
+namespace {
 
+void unload_hip_module(ihipModule_t* module, int device) {
+  if(module) {
+    hip_device_manager::get().activate_device(device);
 
-hip_executable_object::~hip_executable_object() {
-  if(_module) {
-    hip_device_manager::get().activate_device(_device);
-
-    auto err = hipModuleUnload(_module);
+    auto err = hipModuleUnload(module);
 
     if(err != hipSuccess) {
       register_error(
@@ -52,50 +53,72 @@ hip_executable_object::~hip_executable_object() {
   }
 }
 
-hip_executable_object::hip_executable_object(hcf_object_id origin,
-                                             const std::string &target,
-                                             const std::string &hip_fat_binary,
-                                             int device)
+result build_hip_module(ihipModule_t *&module, int device,
+                        const std::string &hip_fat_binary) {
+  // It's unclear if this is actually needed for HIP?
+  hip_device_manager::get().activate_device(device);
+
+  auto err = hipModuleLoadData(&module, hip_fat_binary.c_str());
+
+  if(err == hipSuccess)
+    return make_success();
+  else {
+    return make_error(
+        __hipsycl_here(),
+        error_info{"hip_executable_object: could not create module",
+                   error_code{"HIP", static_cast<int>(err)}});
+  }
+}
+}
+
+hip_multipass_executable_object::~hip_multipass_executable_object() {
+  unload_hip_module(_module, _device);
+}
+
+hip_multipass_executable_object::hip_multipass_executable_object(
+    hcf_object_id origin, const std::string &target,
+    const std::string &hip_fat_binary, int device)
     : _origin{origin}, _target{target}, _device{device}, _module{nullptr} {
   _build_result = build(hip_fat_binary);
 }
 
-result hip_executable_object::get_build_result() const {
+result hip_multipass_executable_object::get_build_result() const {
   return _build_result;
 }
 
-code_object_state hip_executable_object::state() const {
+code_object_state hip_multipass_executable_object::state() const {
   return code_object_state::executable;
 }
 
-code_format hip_executable_object::format() const {
+code_format hip_multipass_executable_object::format() const {
   return code_format::native_isa;
 }
 
-backend_id hip_executable_object::managing_backend() const {
+backend_id hip_multipass_executable_object::managing_backend() const {
   return backend_id::hip;
 }
 
-hcf_object_id hip_executable_object::hcf_source() const {
+hcf_object_id hip_multipass_executable_object::hcf_source() const {
   return _origin;
 }
 
-std::string hip_executable_object::target_arch() const {
+std::string hip_multipass_executable_object::target_arch() const {
   return _target;
 }
 
-compilation_flow hip_executable_object::source_compilation_flow() const {
+compilation_flow hip_multipass_executable_object::source_compilation_flow() const {
   return compilation_flow::explicit_multipass;
 }
 
 std::vector<std::string>
-hip_executable_object::supported_backend_kernel_names() const {
+hip_multipass_executable_object::supported_backend_kernel_names() const {
   // TODO This is currently not easily implementable. Should we throw an
   // exception? Remove it from the general interface?
   return {};
 }
 
-bool hip_executable_object::contains(const std::string &backend_kernel_name) const {
+bool hip_multipass_executable_object::contains(
+    const std::string &backend_kernel_name) const {
   if(!_build_result.is_success())
     return false;
   
@@ -108,28 +131,84 @@ bool hip_executable_object::contains(const std::string &backend_kernel_name) con
   return err == hipSuccess;
 }
 
-hipModule_t hip_executable_object::get_module() const {
+hipModule_t hip_multipass_executable_object::get_module() const {
   return _module;
 }
 
-int hip_executable_object::get_device() const {
+int hip_multipass_executable_object::get_device() const {
   return _device;
 }
 
-result hip_executable_object::build(const std::string& hip_fat_binary) {
-  // It's unclear if this is actually needed for HIP?
-  hip_device_manager::get().activate_device(_device);
+result hip_multipass_executable_object::build(const std::string& hip_fat_binary) {
+  return build_hip_module(_module, _device, hip_fat_binary);
+}
 
-  auto err = hipModuleLoadData(&_module, hip_fat_binary.c_str());
+hip_sscp_executable_object::hip_sscp_executable_object(
+    const std::string &code_image, const std::string &target_arch,
+    hcf_object_id hcf_source, const std::vector<std::string> &kernel_names,
+    int device, const glue::kernel_configuration &config)
+    : _target{target_arch}, _origin{hcf_source}, _kernel_names{kernel_names},
+      _device{device}, _id{config.generate_id()}, _module{nullptr} {
+  _build_result = build(code_image);
+}
 
-  if(err == hipSuccess)
-    return make_success();
-  else {
-    return make_error(
-        __hipsycl_here(),
-        error_info{"hip_executable_object: could not create module",
-                   error_code{"HIP", static_cast<int>(err)}});
+hip_sscp_executable_object::~hip_sscp_executable_object() {
+  unload_hip_module(_module, _device);
+}
+
+result hip_sscp_executable_object::get_build_result() const {
+  return _build_result;
+}
+
+code_object_state hip_sscp_executable_object::state() const {
+  return _build_result.is_success() ? code_object_state::executable
+                                    : code_object_state::invalid;
+}
+
+code_format hip_sscp_executable_object::format() const {
+  return code_format::native_isa;
+}
+
+backend_id hip_sscp_executable_object::managing_backend() const {
+  return backend_id::hip;
+}
+
+hcf_object_id hip_sscp_executable_object::hcf_source() const {
+  return _origin;
+}
+
+std::string hip_sscp_executable_object::target_arch() const {
+  return _target;
+}
+
+compilation_flow hip_sscp_executable_object::source_compilation_flow() const {
+  return compilation_flow::sscp;
+}
+
+std::vector<std::string>
+hip_sscp_executable_object::supported_backend_kernel_names() const {
+  return _kernel_names;
+}
+
+bool hip_sscp_executable_object::contains(
+    const std::string &backend_kernel_name) const {
+  for(const auto& name : _kernel_names) {
+    if(name == backend_kernel_name)
+      return true;
   }
+  return false;
+}
+
+ihipModule_t* hip_sscp_executable_object::get_module() const {
+  return _module;
+}
+
+int hip_sscp_executable_object::get_device() const {
+  return _device;
+}
+
+result hip_sscp_executable_object::build(const std::string& hip_fat_binary) {
+  return build_hip_module(_module, _device, hip_fat_binary);
 }
 
 }

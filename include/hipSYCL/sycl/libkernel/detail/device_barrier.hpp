3889aef495be69a54ef7fc2256a45253c286c11a
Aksel Alpay <aksel.alpay@uni-heidelberg.de>
2022-12-15 04:00:23 +0100

    [SSCP] Map nd_item::barrier() to __hipsycl_sscp_work_group_barrier()

--- include/hipSYCL/sycl/libkernel/detail/device_barrier.hpp
+++ include/hipSYCL/sycl/libkernel/detail/device_barrier.hpp
@@ -33,39 +33,61 @@
 #include "hipSYCL/sycl/libkernel/backend.hpp"
 #include "hipSYCL/sycl/access.hpp"
 
+#if HIPSYCL_LIBKERNEL_IS_DEVICE_PASS_SSCP
+#include "hipSYCL/sycl/libkernel/sscp/builtins/barrier.hpp"
+#endif
+
 namespace hipsycl {
 namespace sycl {
 namespace detail {
+
+#if HIPSYCL_LIBKERNEL_IS_DEVICE_PASS_SPIRV
+
+inline void spirv_barrier(access::fence_space space) {
+  uint32_t flags =  0;
+  
+  if (space == access::fence_space::global_space) {
+    flags = static_cast<uint32_t>(
+                  __spv::MemorySemanticsMask::SequentiallyConsistent |
+                  __spv::MemorySemanticsMask::CrossWorkgroupMemory);
+  } else if (space == access::fence_space::local_space){
+    flags = static_cast<uint32_t>(
+                        __spv::MemorySemanticsMask::SequentiallyConsistent |
+                        __spv::MemorySemanticsMask::WorkgroupMemory);
+  } else {
+    flags = static_cast<uint32_t>(__spv::MemorySemanticsMask::SequentiallyConsistent |
+                      __spv::MemorySemanticsMask::CrossWorkgroupMemory |
+                      __spv::MemorySemanticsMask::WorkgroupMemory);
+  }
+  __spirv_ControlBarrier(__spv::Scope::Workgroup, __spv::Scope::Workgroup,
+                          flags);
+}
+
+#endif
+
+#if HIPSYCL_LIBKERNEL_IS_DEVICE_PASS_SSCP
+inline void sscp_barrier(access::fence_space space) {
+  if(space == access::fence_space::local_space) {
+    __hipsycl_sscp_work_group_barrier(memory_scope::work_group,
+                                      memory_order::relaxed);
+  } else {
+    __hipsycl_sscp_work_group_barrier(memory_scope::device,
+                                      memory_order::relaxed);
+  }
+}
+
+#endif
 
 HIPSYCL_KERNEL_TARGET
 inline void local_device_barrier(
     access::fence_space space = access::fence_space::global_and_local) {
 
-  __hipsycl_if_target_hiplike(
-    __syncthreads();
-  );
-  __hipsycl_if_target_spirv(
-    uint32_t flags =  0;
-    
-    if (space == access::fence_space::global_space) {
-      flags = static_cast<uint32_t>(
-                    __spv::MemorySemanticsMask::SequentiallyConsistent |
-                    __spv::MemorySemanticsMask::CrossWorkgroupMemory);
-    } else if (space == access::fence_space::local_space){
-      flags = static_cast<uint32_t>(
-                          __spv::MemorySemanticsMask::SequentiallyConsistent |
-                          __spv::MemorySemanticsMask::WorkgroupMemory);
-    } else {
-      flags = static_cast<uint32_t>(__spv::MemorySemanticsMask::SequentiallyConsistent |
-                        __spv::MemorySemanticsMask::CrossWorkgroupMemory |
-                        __spv::MemorySemanticsMask::WorkgroupMemory);
-    }
-    __spirv_ControlBarrier(__spv::Scope::Workgroup, __spv::Scope::Workgroup,
-                            flags);
-  );
-  __hipsycl_if_target_host(
-    assert(false && "device barrier called on CPU, this should not happen");
-  );
+  __hipsycl_backend_switch(
+      assert(false && "device barrier called on CPU, this should not happen"), 
+      sscp_barrier(space),
+      __syncthreads(),
+      __syncthreads(),
+      spirv_barrier(space));
 }
 
 }

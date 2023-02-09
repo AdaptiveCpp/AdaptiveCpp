29a3c7e4beaf2e51c93b8491b257817f6f2a95da
Aksel Alpay <aksel.alpay@uni-heidelberg.de>
2023-02-09 19:20:15 +0100

    [half] Fix amdgpu sscp half implementation

--- include/hipSYCL/sycl/libkernel/detail/half_representation.hpp
+++ include/hipSYCL/sycl/libkernel/detail/half_representation.hpp
@@ -40,8 +40,7 @@
 #if (defined(__x86_64__) && defined(HIPSYCL_ENABLE_HALF_ON_HOST)) ||           \
     (defined(__arm__) && defined(HIPSYCL_ENABLE_HALF_ON_HOST)) ||              \
     (defined(__aarch64__) && defined(HIPSYCL_ENABLE_HALF_ON_HOST)) ||          \
-    ((defined(__AMDGPU__) || defined(__SPIR__) || defined(__SPIR64__)) &&      \
-     HIPSYCL_LIBKERNEL_IS_DEVICE_PASS)
+    (defined(__AMDGPU__) || defined(__SPIR__) || defined(__SPIR64__))
   // These targets support _Float16
   #define HIPSYCL_HALF_HAS_FLOAT16_TYPE
 #endif

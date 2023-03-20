fa0ad698cc468c53130068cd3bcbfbc0a45105cf
Aksel Alpay <aksel.alpay@uni-heidelberg.de>
2023-03-20 21:15:15 +0100

    [Renaming] Support new name in cmake integration (#958)
    * Add initial Open SYCL cmake integration
    
    * [Renaming] Move tests and CI to new cmake integration and fix bugs
    
    * [Renaming] Migrate LIT tests configuration
    
    * [Renaming] Adapt LIT commands
    
    * [Renaming] Add deprecation notice to find_package(hipSYCL)

--- tests/compiler/reduce_for.cpp
+++ tests/compiler/reduce_for.cpp
@@ -1,6 +1,6 @@
-// RUN: %syclcc %s -o %t --hipsycl-targets=omp --hipsycl-use-accelerated-cpu
+// RUN: %syclcc %s -o %t --opensycl-targets=omp --opensycl-use-accelerated-cpu
 // RUN: %t | FileCheck %s
-// RUN: %syclcc %s -o %t --hipsycl-targets=omp --hipsycl-use-accelerated-cpu -O
+// RUN: %syclcc %s -o %t --opensycl-targets=omp --opensycl-use-accelerated-cpu -O
 // RUN: %t | FileCheck %s
 
 #include <iostream>

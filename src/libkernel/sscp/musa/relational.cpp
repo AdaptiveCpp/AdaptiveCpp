a81c6f88029d3c5dac0d0788d1174b83da015a89
fxzjshm <fxzjshm@163.com>
2023-07-31 14:01:08 +0800

    [sscp] musa: fix int width in builtin interface
    related to caee556 ("Use fixed width int types in SSCP builtin interface")

--- src/libkernel/sscp/musa/relational.cpp
+++ src/libkernel/sscp/musa/relational.cpp
@@ -30,11 +30,11 @@
 
 
 #define HIPSYCL_SSCP_MAP_MTML_REL_BUILTIN(name)                                \
-  HIPSYCL_SSCP_BUILTIN int __hipsycl_sscp_##name##_f32(float x) {              \
-    return __mt_##name##_f32(x);                                             \
+  HIPSYCL_SSCP_BUILTIN __hipsycl_int32 __hipsycl_sscp_##name##_f32(float x) {  \
+    return __mt_##name##_f32(x);                                               \
   }                                                                            \
-  HIPSYCL_SSCP_BUILTIN int __hipsycl_sscp_##name##_f64(double x) {             \
-    return __mt_##name##_f64(x);                                             \
+  HIPSYCL_SSCP_BUILTIN __hipsycl_int32 __hipsycl_sscp_##name##_f64(double x) { \
+    return __mt_##name##_f64(x);                                               \
   }
   
 HIPSYCL_SSCP_MAP_MTML_REL_BUILTIN(isnan)

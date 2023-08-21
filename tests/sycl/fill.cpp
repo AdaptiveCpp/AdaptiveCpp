76e63da2ecafdc6a922c8964ce6d71d7bc059fdb
Nils Friess <nils.friess@gmail.com>
2023-08-21 14:52:32 +0200

    Extend fill tests to check accessors with offsets

--- tests/sycl/fill.cpp
+++ tests/sycl/fill.cpp
@@ -29,13 +29,8 @@
 #include "sycl_test_suite.hpp"
 #include <boost/test/unit_test_suite.hpp>
 
-BOOST_FIXTURE_TEST_SUITE(fill_tests, reset_device_fixture)
-
-
-BOOST_AUTO_TEST_CASE_TEMPLATE(fill_buffer, _dimensions,
-  test_dimensions::type) {
-  
-  constexpr auto d = _dimensions::value;
+template <int d>
+void fill_test_helper(cl::sycl::id<d> offset = cl::sycl::id<d>{}) {
   namespace s = cl::sycl;
 
   cl::sycl::queue q;
@@ -53,8 +48,18 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(fill_buffer, _dimensions,
 
   auto fill_value = make_test_value<s::id, d>({3}, {3,3}, {3,3,3});
   q.submit([&](s::handler& cgh){
-    auto buff_acc = buff.template get_access<s::access::mode::write>(cgh);
-    cgh.fill(buff_acc, fill_value);
+    if (offset != s::id<d>{}) {
+      auto range = buff_size;
+      for (int i = 0; i < d; ++i)
+        range[i] -= offset[i];
+
+      auto buff_acc = buff.template get_access<s::access::mode::write>(cgh, range, offset);
+      cgh.fill(buff_acc, fill_value);
+    } else {
+      auto buff_acc = buff.template get_access<s::access::mode::write>(cgh);
+      cgh.fill(buff_acc, fill_value);
+    }
+
   });
 
   auto buff_host = buff.template get_access<s::access::mode::read>();
@@ -62,13 +67,38 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(fill_buffer, _dimensions,
   size_t j_validation_range = (d >= 2) ? buff_size[1] : 1;
   size_t k_validation_range = (d == 3) ? buff_size[2] : 1;
 
+  const auto after_offset = [&](s::id<d> idx) {
+    if constexpr (d == 1)
+      return offset[0] <= idx[0];
+    if constexpr (d == 2) 
+      return (offset[0] <= idx[0]) && (offset[1] <= idx[1]);
+    if constexpr (d == 3)
+      return (offset[0] <= idx[0]) && (offset[1] <= idx[1]) && (offset[2] <= idx[2]);
+  };
+
   for(size_t i = 0; i < buff_size[0]; ++i)
     for(size_t j = 0; j < j_validation_range; ++j)
       for(size_t k = 0; k < k_validation_range; ++k)
-      {
+      {        
         auto idx = make_test_value<s::id, d>({i}, {i,j}, {i,j,k});
-        assert_array_equality(buff_host[idx], fill_value);
+        if (after_offset(idx))
+          assert_array_equality(buff_host[idx], fill_value);
       }
+}
+
+BOOST_FIXTURE_TEST_SUITE(fill_tests, reset_device_fixture)
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(fill_buffer, _dimensions,
+  test_dimensions::type) {
+
+  fill_test_helper<_dimensions::value>();
+}
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(fill_with_offset, _dimensions,
+                              test_dimensions::type) {
+  namespace s = cl::sycl;
+  auto offset = make_test_value<s::id, _dimensions::value>({8}, {8, 8}, {8, 8, 8});
+  fill_test_helper<_dimensions::value>(offset);
 }
 
 BOOST_AUTO_TEST_SUITE_END()

d6044a8810cd8c2c974e3eae7ca96e867b5c4cc2
Nils Friess <nils.friess@gmail.com>
2023-08-23 15:54:32 +0200

    [pstl] Add tests for `calloc` and `aligned_alloc`

--- tests/pstl/memory.cpp
+++ tests/pstl/memory.cpp
@@ -45,4 +45,37 @@ BOOST_AUTO_TEST_CASE(pstl_global_malloc_free) {
   free(p1);
 }
 
+BOOST_AUTO_TEST_CASE(pstl_calloc) {
+  constexpr std::size_t size = 64;
+
+  int *p1 = reinterpret_cast<int *>(std::calloc(size, sizeof(int)));
+
+  std::vector<int> host_data(size, 123);
+  std::copy(std::execution::par_unseq, p1, p1 + size,
+            host_data.begin());
+
+  for (std::size_t i = 0; i < size; ++i)
+    BOOST_CHECK_EQUAL(host_data[i], 0);
+
+  std::free(p1);
+}
+
+BOOST_AUTO_TEST_CASE(pstl_aligned_alloc) {
+  constexpr std::size_t alignment = 8;
+  constexpr std::size_t size = alignment * 8;
+  constexpr int value = 42;
+
+  int *p1 = reinterpret_cast<int *>(std::aligned_alloc(alignment, size*sizeof(int)));
+
+  std::fill(std::execution::par_unseq, p1, p1 + size, value);
+
+  std::vector<int> host_data(size);
+  std::fill(host_data.begin(), host_data.end(), value);
+
+  for(std::size_t i = 0; i < size; ++i)
+    BOOST_CHECK_EQUAL(host_data[i], p1[i]);
+
+  std::free(p1);
+}
+
 BOOST_AUTO_TEST_SUITE_END()

5d3ad7a44db2811521a2ebcdc6e4f29fb126eaf5
Aksel Alpay <aksel.alpay@uni-heidelberg.de>
2023-09-20 02:32:10 +0200

    [Renaming] Add AdaptiveCpp cmake infrastructure, migrate tests to use new cmake integration (#1144)
    * [Renaming] Migrate syclcc flags and content to new name; add acpp alias
    
    * Make acpp the primary compilation driver; let cmake copy it to syclcc and syclcc-clang names during install for compatibility
    
    * [renaming] Add AdaptiveCpp cmake integration; migrate tests
    
    * Fix typo for AdaptiveCpp cmake integration causing compiler launch failure
    
    * Use ACPP_COMPILER_LAUNCH_RULE universally
    
    * [renaming] Migrate LIT tests to new name

--- tests/compiler/lit.cfg.py
+++ tests/compiler/lit.cfg.py
@@ -1,7 +1,7 @@
 import lit.formats
 import os
 
-config.name = 'Open SYCL Plugin'
+config.name = 'AdaptiveCpp Plugin'
 config.test_format = lit.formats.ShTest(True)
 
 config.suffixes = ['.c', '.cpp', '.cc']
@@ -9,7 +9,7 @@ config.suffixes = ['.c', '.cpp', '.cc']
 config.test_source_root = os.path.dirname(__file__)
 config.test_exec_root = os.path.join(config.my_obj_root, 'test')
 
-config.substitutions.append(('%syclcc', config.opensycl_syclcc))
+config.substitutions.append(('%acpp', config.acpp_compiler))
 
 if "HIPSYCL_DEBUG_LEVEL" in os.environ:
   config.environment["HIPSYCL_DEBUG_LEVEL"] = os.environ["HIPSYCL_DEBUG_LEVEL"]

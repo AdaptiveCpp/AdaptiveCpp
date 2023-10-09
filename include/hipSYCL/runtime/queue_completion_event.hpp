0fb8eaee42aaad4fda3e59cb5e5bf17291fb67ac
Aksel Alpay <aksel.alpay@uni-heidelberg.de>
2023-10-09 20:00:09 +0200

    Make coarse-grained event decay thread-safe

--- include/hipSYCL/runtime/queue_completion_event.hpp
+++ include/hipSYCL/runtime/queue_completion_event.hpp
@@ -28,6 +28,7 @@
 #ifndef HIPSYCL_QUEUE_COMPLETION_EVENT_HPP
 #define HIPSYCL_QUEUE_COMPLETION_EVENT_HPP
 
+#include <mutex>
 #include "error.hpp"
 #include "hipSYCL/runtime/event.hpp"
 #include "inorder_queue_event.hpp"
@@ -74,15 +75,19 @@ public:
     _is_complete = true;
   }
 
-  // This function is currently not thread-safe and should not be
-  // invoked by multiple threads
   virtual FineGrainedBackendEventT request_backend_event() override {
     if(!_has_fine_grained_event){
-      // We need to first create an actual fine-grained event
-      // to be able to service the request, since this event
-      // is not tied to a backend event otherwise.
-      _fine_grained_event = _q->insert_event();
-      _has_fine_grained_event = true;
+      // Avoid another thread from also entering this code path -
+      // setting _has_fined_grained_event to true and setting
+      // _fine_grained_event must happend atomically.
+      std::lock_guard<std::mutex> lock{_decay_to_fine_grained_evt_mutex};
+      if(!_has_fine_grained_event) {
+        // We need to first create an actual fine-grained event
+        // to be able to service the request, since this event
+        // is not tied to a backend event otherwise.
+        _fine_grained_event = _q->insert_event();
+        _has_fine_grained_event = true;
+      }
     }
     
     return static_cast<inorder_queue_event<FineGrainedBackendEventT> *>(
@@ -94,6 +99,7 @@ private:
   std::atomic<bool> _has_fine_grained_event;
   std::atomic<bool> _is_complete;
   std::shared_ptr<dag_node_event> _fine_grained_event;
+  std::mutex _decay_to_fine_grained_evt_mutex;
 };
 
 }

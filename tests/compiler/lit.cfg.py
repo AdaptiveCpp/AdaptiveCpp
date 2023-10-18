98fafc351bfd367ffec8ad166b55495c35c1b541
Aksel Alpay <aksel.alpay@uni-heidelberg.de>
2023-10-19 00:14:05 +0200

    [CI] Expand LIT infrastructure to SSCP and add some SSCP CI tests (#1196)
    * [SSCP] Add LIT test infrastructure and enable in CI via CPU OpenCL
    
    * [SSCP][LIT] Add test case for circular globals
    
    * [SSCP][LIT] Also test with -g
    
    * Add some diagnostic output
    
    * Install intel-oneapi-compilers package
    
    * Only select SSCP-capable devices
    
    * Cleanup SSCP LIT tests
    
    * Ensure SSCP LIT common.hpp is only included once
    
    * [CI] Enable clang 17 for SSCP LIT

--- tests/compiler/lit.cfg.py
+++ tests/compiler/lit.cfg.py
@@ -7,7 +7,7 @@ config.test_format = lit.formats.ShTest(True)
 config.suffixes = ['.c', '.cpp', '.cc']
 
 config.test_source_root = os.path.dirname(__file__)
-config.test_exec_root = os.path.join(config.my_obj_root, 'test')
+config.test_exec_root = os.path.join(config.my_obj_root)
 
 config.substitutions.append(('%acpp', config.acpp_compiler))
 

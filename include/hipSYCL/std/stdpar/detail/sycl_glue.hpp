04ea2bced95c41f467d01a398612afdc22e42f3e
Nils Friess <nils.friess@gmail.com>
2023-10-25 16:29:43 +0200

    Remove names of unused arguments to avoid warnings

--- include/hipSYCL/std/stdpar/detail/sycl_glue.hpp
+++ include/hipSYCL/std/stdpar/detail/sycl_glue.hpp
@@ -313,56 +313,56 @@ void operator delete[]( void* ptr ) noexcept {
 }
 
 HIPSYCL_STDPAR_FREE
-void operator delete  ( void* ptr, std::align_val_t al ) noexcept {
+void operator delete  ( void* ptr, std::align_val_t ) noexcept {
   hipsycl::stdpar::unified_shared_memory::free(ptr);
 }
 
 HIPSYCL_STDPAR_FREE
-void operator delete[]( void* ptr, std::align_val_t al ) noexcept {
+void operator delete[]( void* ptr, std::align_val_t ) noexcept {
   hipsycl::stdpar::unified_shared_memory::free(ptr);
 }
 
 HIPSYCL_STDPAR_FREE
-void operator delete  ( void* ptr, std::size_t sz ) noexcept {
+void operator delete  ( void* ptr, std::size_t ) noexcept {
   hipsycl::stdpar::unified_shared_memory::free(ptr);
 }
 
 HIPSYCL_STDPAR_FREE
-void operator delete[]( void* ptr, std::size_t sz ) noexcept {
+void operator delete[]( void* ptr, std::size_t ) noexcept {
   hipsycl::stdpar::unified_shared_memory::free(ptr);
 }
 
 HIPSYCL_STDPAR_FREE
-void operator delete  ( void* ptr, std::size_t sz,
-                        std::align_val_t al ) noexcept {
+void operator delete  ( void* ptr, std::size_t,
+                        std::align_val_t ) noexcept {
   hipsycl::stdpar::unified_shared_memory::free(ptr);
 }
 
 HIPSYCL_STDPAR_FREE
-void operator delete[]( void* ptr, std::size_t sz,
-                        std::align_val_t al ) noexcept {
+void operator delete[]( void* ptr, std::size_t,
+                        std::align_val_t ) noexcept {
   hipsycl::stdpar::unified_shared_memory::free(ptr);
 }
 
 HIPSYCL_STDPAR_FREE
-void operator delete  ( void* ptr, const std::nothrow_t& tag ) noexcept {
+void operator delete  ( void* ptr, const std::nothrow_t& ) noexcept {
   hipsycl::stdpar::unified_shared_memory::free(ptr);
 }
 
 HIPSYCL_STDPAR_FREE
-void operator delete[]( void* ptr, const std::nothrow_t& tag ) noexcept {
+void operator delete[]( void* ptr, const std::nothrow_t& ) noexcept {
   hipsycl::stdpar::unified_shared_memory::free(ptr);
 }
 
 HIPSYCL_STDPAR_FREE
-void operator delete  ( void* ptr, std::align_val_t al,
-                        const std::nothrow_t& tag ) noexcept {
+void operator delete  ( void* ptr, std::align_val_t,
+                        const std::nothrow_t& ) noexcept {
   hipsycl::stdpar::unified_shared_memory::free(ptr);
 }
 
 HIPSYCL_STDPAR_FREE
-void operator delete[]( void* ptr, std::align_val_t al,
-                        const std::nothrow_t& tag ) noexcept {
+void operator delete[]( void* ptr, std::align_val_t,
+                        const std::nothrow_t& ) noexcept {
   hipsycl::stdpar::unified_shared_memory::free(ptr);
 }
 

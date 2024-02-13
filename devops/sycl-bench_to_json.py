21107f825fff464f347ff38f3d2e583ab3d07146
Nils Friess <nils.friess@gmail.com>
2024-02-13 16:41:57 +0100

    Use SYCL bench's run-suite script to run benchmarks

--- devops/sycl-bench_to_json.py
+++ devops/sycl-bench_to_json.py
@@ -1,49 +1,41 @@
-import subprocess
+import csv
 import sys
+import os
+import json
 
-result = subprocess.run(sys.argv[1:], stdout=subprocess.PIPE)
-if result.returncode == 0:
-    names = []
-    runtime_mean = []
-    runtime_stddev = []
+if len(sys.argv) < 2:
+    print(f"usage: {os.path.basename(__file__)} SYCL_BENCH_OUTPUT.CSV")
+    exit(1)
+
+with open(sys.argv[1], newline='') as csvfile:
+    reader = csv.reader(csvfile)
     
+    # Find out which entries are run-time-mean and run-time-stddev
+    first_row = next(reader)
+    mean_idx   = -1
+    stddev_idx = -1
+    curr_idx   = 0
+    for entry in first_row:
+        if entry == 'run-time-mean':
+            mean_idx = curr_idx
+        if entry == 'run-time-stddev':
+            stddev_idx = curr_idx
+        curr_idx += 1
+    assert(mean_idx != -1)
+    assert(stddev_idx != -1)
 
-    for line in result.stdout.decode("utf-8").splitlines():
-        if line.startswith("****"):
-            # The name of the benchmark is always printed as ***** name*****
-            names.append([s for s in line.split("*") if s != ''][0].strip())
-
-        if line.startswith("run-time-mean"):
-            runtime_mean.append(line.split(" ")[1])
-
-        if line.startswith("run-time-stddev"):
-            runtime_stddev.append(line.split(" ")[1])
-            
-    assert(len(names) == len(runtime_mean))
-    assert(len(names) == len(runtime_stddev))
-
-    jsonstring = ""
-    jsonstring += "["
-
-    for entry in zip(names, runtime_mean, runtime_stddev):
-        jsonstring += "{"
-
-        jsonstring += "\"name\":"
-        jsonstring += "\"" + entry[0] + "\""
-        jsonstring += ","
-
-        jsonstring += "\"unit\":\"s\","
-
-        jsonstring += "\"value\":"
-        jsonstring += entry[1]
-        jsonstring += ","
-
-        jsonstring += "\"range\":"
-        jsonstring += entry[2]
-
-        jsonstring += "},"
-
-    jsonstring = jsonstring[:-1]
-    jsonstring += "]"
-
-    print(jsonstring)
+    benchmarks = list()
+    for row in reader:
+        if row[0][0] == '#':
+            continue
+        
+        benchmark = dict()
+        benchmark['name']  = row[0]
+        benchmark['unit']  = 's'
+        benchmark['value'] = row[mean_idx]
+        benchmark['range'] = row[stddev_idx]
+    
+        benchmarks.append(benchmark)
+    
+    with open("output.json", "w") as outfile:
+        json.dump(benchmarks, outfile, indent=2)

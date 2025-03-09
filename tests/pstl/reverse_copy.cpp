98c228b0adab8f5707785476854fe3a3c40f29ab
Sanchi Vaishnavi <vaishnavi.sanchi@gmail.com>
2025-03-09 11:27:19 +0100

    Use MP11 in tests

--- tests/pstl/reverse_copy.cpp
+++ tests/pstl/reverse_copy.cpp
@@ -15,7 +15,8 @@
 #include <vector>
 
 #include <boost/test/unit_test.hpp>
-#include <boost/mpl/list.hpp>
+#include <boost/mp11/list.hpp>
+#include <boost/mp11/mpl.hpp>
 
 #include "pstl_test_suite.hpp"
 
@@ -43,28 +44,28 @@ void test_reverse_copy(Policy&& pol, std::size_t problem_size) {
   BOOST_CHECK(data == dest_device);
 }
 
-using types = boost::mpl::list<int, non_trivial_copy>;
-BOOST_AUTO_TEST_CASE_TEMPLATE(par_unseq_empty, T, types::type) {
+using types = boost::mp11::mp_list<int, non_trivial_copy>;
+BOOST_AUTO_TEST_CASE_TEMPLATE(par_unseq_empty, T, types) {
   test_reverse_copy<T>(std::execution::par_unseq, 0);
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(par_unseq_single_element, T, types::type) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(par_unseq_single_element, T, types) {
   test_reverse_copy<T>(std::execution::par_unseq, 1);
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(par_unseq_medium_size, T, types::type) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(par_unseq_medium_size, T, types) {
   test_reverse_copy<T>(std::execution::par_unseq, 10);
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(par_empty, T, types::type) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(par_empty, T, types) {
   test_reverse_copy<T>(std::execution::par, 0);
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(par_single_element, T, types::type) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(par_single_element, T, types) {
   test_reverse_copy<T>(std::execution::par, 1);
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(par_medium_size, T, types::type) {
+BOOST_AUTO_TEST_CASE_TEMPLATE(par_medium_size, T, types) {
   test_reverse_copy<T>(std::execution::par, 10);
 }
 BOOST_AUTO_TEST_SUITE_END()

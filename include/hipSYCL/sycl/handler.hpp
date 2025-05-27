6b0a30c41d6716cb6d3d7a08d7f3b8ba1ad53f64
ath0mos <athanasios.tsarapatsanis@outlook.de>
2025-05-27 15:22:55 +0200

    Fixed if check

--- include/hipSYCL/sycl/handler.hpp
+++ include/hipSYCL/sycl/handler.hpp
@@ -338,7 +338,7 @@ public:
                     id<dimensions> workItemOffset,
                     const ReductionsAndKernel &... redu_kernel) {
 
-    if(numWorkItems == 0)
+    if(numWorkItems.size() == 0)
       AdaptiveCpp_enqueue_custom_operation([](auto&){});
 
     else {
@@ -370,6 +370,7 @@ public:
           kernel, reductions...);
       };
     }
+
     detail::separate_last_argument_and_apply(invoker, redu_kernel...);
   }
 

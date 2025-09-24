7dc7a305b080306b7877e661a10ac3cde814e2aa
ath0mos <athanasios.tsarapatsanis@outlook.de>
2025-09-24 18:09:29 +0200

    changed macro

--- include/hipSYCL/sycl/libkernel/sub_group.hpp
+++ include/hipSYCL/sycl/libkernel/sub_group.hpp
@@ -146,7 +146,7 @@ private:
 
   ACPP_KERNEL_TARGET
   id_type get_subgroup_id() const {
-  __acpp_if_target_device(
+    __acpp_if_target_hiplike(
     int local_tid = __acpp_lid_x 
                   + __acpp_lid_y * __acpp_lsize_x 
                   + __acpp_lid_z * __acpp_lsize_x * __acpp_lsize_y;
@@ -157,11 +157,11 @@ private:
 
   ACPP_KERNEL_TARGET
   id_type get_num_subgroups() const {
-  __acpp_if_target_device(
+    __acpp_if_target_hiplike(
     auto wg_size = __acpp_lsize_x *
                    __acpp_lsize_y *
                    __acpp_lsize_z;
-    auto sg_size = __acpp_warp_size; // max subgroup size
+    auto sg_size = __acpp_warp_size; // max subgroup size 
 
     return (wg_size + sg_size - 1) / sg_size;
   );

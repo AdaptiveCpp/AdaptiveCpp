84b3cd46dd1d95d4801af91299a65413f2dad5a4
Alexey Ozeritskiy <aozeritsky@gmail.com>
2026-02-19 22:09:34 +0000

    Assertion for metal version of __acpp_sscp_print

--- src/libkernel/sscp/metal/print.cpp
+++ src/libkernel/sscp/metal/print.cpp
@@ -11,5 +11,9 @@
 
 #include "hipSYCL/sycl/libkernel/sscp/builtins/print.hpp"
 
+HIPSYCL_SSCP_BUILTIN void __acpp_sscp_metal_symbol_assert(const char* s);
+
 void __acpp_sscp_print(const char* msg) {
+  //TODO: use <metal_logging>
+  __acpp_sscp_metal_symbol_assert("__builtin_trap()");
 }

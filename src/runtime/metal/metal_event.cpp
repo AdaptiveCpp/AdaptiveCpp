2f1313edcd6c588c283217ad9dbfb02d8d2913af
Alexey Ozeritskiy <aozeritsky@gmail.com>
2026-03-09 15:51:50 +0000

    [Metal] Use native events (#1989)
    * [Metal] Use native events
    
    * Add test
    
    Co-authored-by: Andrey Alekseenko <al42and@gmail.com>
    
    ---------
    
    Co-authored-by: Andrey Alekseenko <al42and@gmail.com>

--- src/runtime/metal/metal_event.cpp
+++ src/runtime/metal/metal_event.cpp
@@ -10,28 +10,34 @@
 // SPDX-License-Identifier: BSD-2-Clause
 #include "hipSYCL/runtime/metal/metal_event.hpp"
 
+#include <Metal/Metal.hpp>
+
+#include <thread>
+
 namespace hipsycl {
 namespace rt {
 
-metal_node_event::metal_node_event()
-  : _signal_channel{std::make_shared<signal_channel>()} {}
+metal_node_event::metal_node_event(metal_event_handle handle)
+  : _handle{handle} {
+  _handle.event->retain();
+}
 
-metal_node_event::~metal_node_event() {}
+metal_node_event::~metal_node_event() {
+  _handle.event->release();
+}
 
 bool metal_node_event::is_complete() const {
-  return _signal_channel->has_signalled();
+  return _handle.event->signaledValue() >= _handle.value;
 }
 
 void metal_node_event::wait() {
-  _signal_channel->wait();
+  while (!_handle.event->waitUntilSignaledValue(_handle.value, 1000)) {
+    std::this_thread::yield();
+  }
 }
 
-std::shared_ptr<signal_channel> metal_node_event::get_signal_channel() const {
-  return _signal_channel;
-}
-
-std::shared_ptr<signal_channel> metal_node_event::request_backend_event() {
-  return get_signal_channel();
+metal_event_handle metal_node_event::request_backend_event() {
+  return _handle;
 }
 
 } // namespace rt

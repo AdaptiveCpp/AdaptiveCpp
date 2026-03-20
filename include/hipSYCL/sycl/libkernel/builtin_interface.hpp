e253aa10321e2fc7f86cd3b13d413eb2e104d688
ferdymercury <ferdymercury@users.noreply.github.com>
2026-03-20 15:42:31 +0100

    Update builtin_interface.hpp

--- include/hipSYCL/sycl/libkernel/builtin_interface.hpp
+++ include/hipSYCL/sycl/libkernel/builtin_interface.hpp
@@ -659,62 +659,62 @@ HIPSYCL_BUILTIN int __acpp_signbit(T x) noexcept {
   HIPSYCL_RETURN_DISPATCH_BUILTIN(__acpp_signbit, x);
 }
 
-// Relational binary functions below default always to host
+// Relational binary functions below are directly implemented here
 // Potential improvement: use HIPSYCL_RETURN_DISPATCH_BUILTIN and specialize for SPIRV as done in
 // https://github.com/intel/llvm/blob/sycl/sycl/include/sycl/detail/builtins/relational_functions.inc#L130
 
 template<class T>
 HIPSYCL_BUILTIN bool __acpp_isequal(T x, T y) noexcept
 {
-    return host_builtins::__acpp_isequal(x, y);
+    return x == y;
 }
 
 template<class T>
 HIPSYCL_BUILTIN bool __acpp_isnotequal(T x, T y) noexcept
 {
-    return host_builtins::__acpp_isnotequal(x, y);
+    return x != y;
 }
 
 template<class T>
 HIPSYCL_BUILTIN bool __acpp_isgreater(T x, T y) noexcept
 {
-    return host_builtins::__acpp_isgreater(x, y);
+    return x > y;
 }
 
 template<class T>
 HIPSYCL_BUILTIN bool __acpp_isgreaterequal(T x, T y) noexcept
 {
-    return host_builtins::__acpp_isgreaterequal(x, y);
+    return x >= y;
 }
 
 template<class T>
 HIPSYCL_BUILTIN bool __acpp_isless(T x, T y) noexcept
 {
-    return host_builtins::__acpp_isless(x, y);
+    return x < y;
 }
 
 template<class T>
 HIPSYCL_BUILTIN bool __acpp_islessequal(T x, T y) noexcept
 {
-    return host_builtins::__acpp_islessequal(x, y);
+    return x <= y;
 }
 
 template<class T>
 HIPSYCL_BUILTIN bool __acpp_islessgreater(T x, T y) noexcept
 {
-    return host_builtins::__acpp_islessgreater(x, y);
+    return (x < y) || (x > y);
 }
 
 template<class T>
 HIPSYCL_BUILTIN bool __acpp_isordered(T x, T y) noexcept
 {
-    return host_builtins::__acpp_isordered(x, y);
+    return __acpp_isequal(x, x) && __acpp_isequal(y, y);
 }
 
 template<class T>
 HIPSYCL_BUILTIN bool __acpp_isunordered(T x, T y) noexcept
 {
-    return host_builtins::__acpp_isunordered(x, y);
+    return __acpp_isnan(x) || __acpp_isnan(y);
 }
 
 }

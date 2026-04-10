4cda112e960e51c483616b904040e646a8157b62
Alexander Schröter <me@alexanderschroeter.net>
2026-04-10 09:30:08 +0200

    fix: update PassPlugin.h include path for LLVM 23+
    In LLVM 23, PassPlugin.h was moved from llvm/Passes/ to a new
    llvm/Plugins/ library as part of a dependency restructure
    (llvm/llvm-project@f54df0d). Add a version guard to use the
    correct path depending on the LLVM version.

--- src/compiler/AdaptiveCppLlvmPasses.cpp
+++ src/compiler/AdaptiveCppLlvmPasses.cpp
@@ -39,7 +39,11 @@
 
 #include "llvm/Pass.h"
 #include "llvm/Passes/PassBuilder.h"
+#if LLVM_VERSION_MAJOR >= 23
+#include "llvm/Plugins/PassPlugin.h"
+#else
 #include "llvm/Passes/PassPlugin.h"
+#endif
 #include "llvm/Support/CommandLine.h"
 
 #if LLVM_VERSION_MAJOR < 16

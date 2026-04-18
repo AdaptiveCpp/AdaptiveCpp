df5ff0efb4a234a1237c7256c2ce9b13a605b70c
ferdymercury <ferdymercury@users.noreply.github.com>
2026-04-18 10:18:51 +0200

    Update include/hipSYCL/sycl/libkernel/vec.hpp
    Co-authored-by: David--Cléris Timothée <timothee.davidcleris@proton.me>

--- include/hipSYCL/sycl/libkernel/vec.hpp
+++ include/hipSYCL/sycl/libkernel/vec.hpp
@@ -339,7 +339,7 @@ public:
     vec<ConvertT, N> result;
 
     for(int i = 0; i < N; ++i) {
-// TODO handle float to float conversion. Here only float to short rounding is implemented
+// TODO handle float to float conversion. Here only float to integer rounding is implemented
       if constexpr (std::is_floating_point_v<T> &&
                        std::is_integral_v<ConvertT>) {
         if constexpr (RM == rounding_mode::rte || RM == rounding_mode::automatic) {

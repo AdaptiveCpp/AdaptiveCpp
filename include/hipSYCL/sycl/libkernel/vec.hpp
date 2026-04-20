e57268a1b424a71463b05a71752115893f5b846e
David--Cléris Timothée <timothee.davidcleris@proton.me>
2026-04-20 18:30:45 +0000

    use acpp builtin instead

--- include/hipSYCL/sycl/libkernel/vec.hpp
+++ include/hipSYCL/sycl/libkernel/vec.hpp
@@ -15,6 +15,7 @@
 #include "half.hpp"
 #include "multi_ptr.hpp"
 #include "bit_cast.hpp"
+#include "builtin_interface.hpp"
 
 #include <cstdint>
 #include <type_traits>
@@ -334,7 +335,32 @@ public:
 
   template <typename ConvertT, rounding_mode RM = rounding_mode::automatic>
   ACPP_UNIVERSAL_TARGET
-  vec<ConvertT, N> convert() const;
+  vec<ConvertT, N> convert() const {
+
+    vec<ConvertT, N> result;
+
+    for(int i = 0; i < N; ++i) {
+      // TODO handle float to float conversion. Here only float to integer rounding is implemented
+      if constexpr (std::is_floating_point_v<T> &&
+                       std::is_integral_v<ConvertT>) {
+        if constexpr (RM == rounding_mode::rte || RM == rounding_mode::automatic) {
+          result[i] = static_cast<ConvertT>(__acpp_rint(_data[i]));
+        } else if constexpr (RM == rounding_mode::rtz) {
+          result[i] = static_cast<ConvertT>(__acpp_trunc(_data[i]));
+        } else if constexpr (RM == rounding_mode::rtp) {
+          result[i] = static_cast<ConvertT>(__acpp_ceil(_data[i]));
+        } else if constexpr (RM == rounding_mode::rtn) {
+          result[i] = static_cast<ConvertT>(__acpp_floor(_data[i]));
+        } else {
+          result[i] = static_cast<ConvertT>(_data[i]);
+        }
+      } else {
+        result[i] = static_cast<ConvertT>(_data[i]);
+      }
+    }
+
+    return result;
+  }
 
   template<typename asT>
   ACPP_UNIVERSAL_TARGET

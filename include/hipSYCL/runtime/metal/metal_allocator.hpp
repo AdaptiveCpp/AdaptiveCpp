c08b436472f1283222ddf73a3af1e80db220009e
Alexey Ozeritskiy <aozeritsky@gmail.com>
2026-09-05 13:30:09 +0100

    [Metal] Make USM allocations resident via MTLResidencySet

--- include/hipSYCL/runtime/metal/metal_allocator.hpp
+++ include/hipSYCL/runtime/metal/metal_allocator.hpp
@@ -21,6 +21,7 @@ namespace MTL {
 
 class Device;
 class Buffer;
+class ResidencySet;
 
 } // namespace MTL
 
@@ -72,19 +73,13 @@ public:
 
   size_t get_delta() const { return _delta; }
 
-  template<typename F>
-  void for_each_buffer(F&& f) const {
-    std::lock_guard<std::mutex> lock{_mutex};
-    for (auto& [ptr, block] : _ptr_to_block) {
-      if (block.buffer) {
-        f(block.buffer);
-      }
-    }
-  }
+  MTL::ResidencySet* get_residency_set() const { return _residency_set; }
 
 private:
   MTL::Buffer* alloc_buffer(size_t size_bytes);
   void calibrate();
+  void add_to_residency_set(MTL::Buffer* buffer);
+  void remove_from_residency_set(MTL::Buffer* buffer);
 
   MTL::Device* _device = nullptr;
   device_id _device_id;
@@ -97,6 +92,7 @@ private:
   };
   std::map<void*, usm_block> _ptr_to_block;
   mutable std::mutex _mutex;
+  MTL::ResidencySet* _residency_set = nullptr;
   std::shared_ptr<metal_mmap_region> _mmap_region;
 };
 

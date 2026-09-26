dcd3a513befab2efd48b5e6839ddc5232e804e0a
Alexey Ozeritskiy <aozeritsky@gmail.com>
2026-09-26 11:42:14 +0100

    [Metal] Add 64-bit sub-group prefix sum/product

--- src/libkernel/sscp/metal/scan_helpers.hpp
+++ src/libkernel/sscp/metal/scan_helpers.hpp
@@ -58,12 +58,59 @@ SCAN_TYPES
 #undef X
 }
 
+// MSL has no 64-bit prefix sum
+// sum 16-bit chunks in 32 bits then add them together to propagate carries
+template <typename T>
+inline T __acpp_sscp_metal_inclusive_scan_sum_64(T value) {
+  const u64 x = (u64)value;
+
+  u32 x0 = (u32)x & 0xffff;
+  u32 x1 = (u32)(x >> 16) & 0xffff;
+  u32 x2 = (u32)(x >> 32) & 0xffff;
+  u32 x3 = (u32)(x >> 48) & 0xffff;
+
+  u32 p0 = __acpp_sscp_metal_scan<u32>("simd_prefix_inclusive_sum", x0);
+  u32 p1 = __acpp_sscp_metal_scan<u32>("simd_prefix_inclusive_sum", x1);
+  u32 p2 = __acpp_sscp_metal_scan<u32>("simd_prefix_inclusive_sum", x2);
+  u32 p3 = __acpp_sscp_metal_scan<u32>("simd_prefix_inclusive_sum", x3);
+
+  u64 prefix = ((u64)p3 << 48) + ((u64)p2 << 32) + ((u64)p1 << 16) + (u64)p0;
+  return (T)prefix;
+}
+
+template <typename T>
+inline T __acpp_sscp_metal_exclusive_scan_sum_64(T value) {
+  const u64 x = (u64)value;
+
+  u32 x0 = (u32)x & 0xffff;
+  u32 x1 = (u32)(x >> 16) & 0xffff;
+  u32 x2 = (u32)(x >> 32) & 0xffff;
+  u32 x3 = (u32)(x >> 48) & 0xffff;
+
+  u32 p0 = __acpp_sscp_metal_scan<u32>("simd_prefix_exclusive_sum", x0);
+  u32 p1 = __acpp_sscp_metal_scan<u32>("simd_prefix_exclusive_sum", x1);
+  u32 p2 = __acpp_sscp_metal_scan<u32>("simd_prefix_exclusive_sum", x2);
+  u32 p3 = __acpp_sscp_metal_scan<u32>("simd_prefix_exclusive_sum", x3);
+
+  u64 prefix = ((u64)p3 << 48) + ((u64)p2 << 32) + ((u64)p1 << 16) + (u64)p0;
+  return (T)prefix;
+}
+
 template <__acpp_sscp_algorithm_op op, typename T>
 inline T __acpp_sscp_sub_group_inclusive_scan(T value) {
   if constexpr (op == __acpp_sscp_algorithm_op::plus) {
-    return __acpp_sscp_metal_scan<T>("simd_prefix_inclusive_sum", value);
+    if constexpr (std::is_integral_v<T> && sizeof(T) == 8) {
+      return __acpp_sscp_metal_inclusive_scan_sum_64<T>(value);
+    } else {
+      return __acpp_sscp_metal_scan<T>("simd_prefix_inclusive_sum", value);
+    }
   } else if constexpr (op == __acpp_sscp_algorithm_op::multiply) {
-    return __acpp_sscp_metal_scan<T>("simd_prefix_inclusive_product", value);
+    if constexpr (std::is_integral_v<T> && sizeof(T) == 8) {
+      using BinaryOp = typename hipsycl::libkernel::sscp::get_op<op>::type;
+      return hipsycl::libkernel::sscp::sg_inclusive_scan(value, BinaryOp{});
+    } else {
+      return __acpp_sscp_metal_scan<T>("simd_prefix_inclusive_product", value);
+    }
   } else if constexpr (
     op == __acpp_sscp_algorithm_op::min ||
     op == __acpp_sscp_algorithm_op::max ||
@@ -82,9 +129,18 @@ inline T __acpp_sscp_sub_group_inclusive_scan(T value) {
 template <__acpp_sscp_algorithm_op op, typename T>
 inline T __acpp_sscp_sub_group_exclusive_scan(T value, T init) {
   if constexpr (op == __acpp_sscp_algorithm_op::plus) {
-    return __acpp_sscp_metal_scan<T>("simd_prefix_exclusive_sum", value);
+    if constexpr (std::is_integral_v<T> && sizeof(T) == 8) {
+      return __acpp_sscp_metal_exclusive_scan_sum_64<T>(value);
+    } else {
+      return __acpp_sscp_metal_scan<T>("simd_prefix_exclusive_sum", value);
+    }
   } else if constexpr (op == __acpp_sscp_algorithm_op::multiply) {
-    return __acpp_sscp_metal_scan<T>("simd_prefix_exclusive_product", value);
+    if constexpr (std::is_integral_v<T> && sizeof(T) == 8) {
+      using BinaryOp = typename hipsycl::libkernel::sscp::get_op<op>::type;
+      return hipsycl::libkernel::sscp::sg_exclusive_scan(value, BinaryOp{}, init);
+    } else {
+      return __acpp_sscp_metal_scan<T>("simd_prefix_exclusive_product", value);
+    }
   } else if constexpr (
     op == __acpp_sscp_algorithm_op::min ||
     op == __acpp_sscp_algorithm_op::max ||
